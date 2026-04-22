65b9cadca63496520537a71825a76598180453d6
pubudu-m <i.pubudumihiranga@gmail.com>
2026-04-22 13:03:16 +0530

    feat: initial project setup mapping cl env

---
+++ circlescare-ios/circlescare-ios/circlescare_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  circlescare_iosApp.swift
+//  circlescare-ios
+//
+//  Created by Pubudu Mihiranga on 2026-04-22.
+//
+
+import SwiftUI
+
+@main
+struct circlescare_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
